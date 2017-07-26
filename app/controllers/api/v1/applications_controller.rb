069f9f8006563049a489e9a8fecef3c185cb8d3f
Yechiel Kalmenson <contact@yechiel.me>
2017-07-26 12:07:12 -0400

    Updated Applications controller to let any user interact with any application

--- app/controllers/api/v1/applications_controller.rb
+++ app/controllers/api/v1/applications_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::ApplicationsController < ApplicationController
 
   def index
 
-    @applications = User.find_by(id: params[:user_id])&.applications
+    @applications = Application.all
     render 'applications/applications.json.jbuilder', applications: @applications
 
   end
@@ -22,42 +22,26 @@ class Api::V1::ApplicationsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(id: params[:user_id])
-    if @user.id == current_user.id
-      @application = @user.applications.new(application_params)
-      if @application.save
-        render 'applications/application.json.jbuilder', application: @application
-      else
-        render json: {
-          errors: @application.errors.full_messages
-        }, status: 500
-      end
+
+    @application = @user.applications.new(application_params)
+    if @application.save
+      render 'applications/application.json.jbuilder', application: @application
     else
       render json: {
-        errors: [
-          {message: "You do not have permission to add applications to this user's dashboard."}
-        ]
-      }, status: 403
+        errors: @application.errors.full_messages
+      }, status: 500
     end
+
   end
 
   def update
-    @user = User.find_by(id: params[:user_id])
-    if @user.id == current_user.id
-      @application = @user.applications.find_by(id: params[:id])
-      if @application.update_attributes(application_params)
-        render 'applications/application.json.jbuilder', application: @application
-      else
-        render json: {
-          errors: @application.errors.full_messages
-        }, status: 500
-      end
+    @application = Application.find_by(id: params[:id])
+    if @application.update_attributes(application_params)
+      render 'applications/application.json.jbuilder', application: @application
     else
       render json: {
-        errors: [
-          {message: "You do not have permission to edit this application."}
-        ]
-      }, status: 403
+        errors: @application.errors.full_messages
+      }, status: 500
     end
   end
 
