592e83f674a1138061b1079989ff44ecc0f7dc16
Yechiel Kalmenson <contact@yechiel.me>
2017-07-26 12:07:22 -0400

    Removed nested routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       root "auth#ping"
-      resources :users, only: [:create, :show] do
+      resources :users, only: [:create, :show]
 
-        resources :applications, only: [:index, :show, :create, :update, :destroy]
+      resources :applications, only: [:index, :show, :create, :update, :destroy]
 
-      end
       post'/auth', to: "auth#login"
       post'/auth/refresh', to: "auth#refresh"
 
