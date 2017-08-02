cba0953eb9fa8e8ff67dad238e4a2064369b303d
Yechiel Kalmenson <contact@yechiel.me>
2017-08-02 16:52:48 -0400

    removed pry from a spec

--- spec/models/auth_spec.rb
+++ spec/models/auth_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Auth, type: :model do
   describe 'JWT authentication' do
     it 'returns a token with the correct format' do
       token = Auth.create_token(1)
-      binding.pry
       expect(token.split(".").length).to eq(3)
     end
   end
