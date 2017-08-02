550688b32d6ff785933c4e523f01166529344459
Yechiel Kalmenson <contact@yechiel.me>
2017-08-02 17:12:25 -0400

    Added spec to make sure Auth can decode a token

--- spec/models/auth_spec.rb
+++ spec/models/auth_spec.rb
@@ -2,9 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Auth, type: :model do
   describe 'JWT authentication' do
+    before(:each) do
+      @token = Auth.create_token(1)
+    end
     it 'returns a token with the correct format' do
-      token = Auth.create_token(1)
-      expect(token.split(".").length).to eq(3)
+
+      expect(@token.split(".").length).to eq(3)
+    end    
+    
+    it 'decodes the token properly' do
+
+      expect(Auth.decode_token(@token)[0]["user_id"]).to eq(1)
     end
   end
 end
