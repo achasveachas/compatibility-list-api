fbc6bf58e6c8a07a9738290830853bfbe1637300
Yechiel Kalmenson <contact@yechiel.me>
2017-08-14 13:37:42 -0400

    Added admin attribute to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426031958) do
+ActiveRecord::Schema.define(version: 20170814172812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,8 +38,9 @@ ActiveRecord::Schema.define(version: 20170426031958) do
     t.string   "username"
     t.string   "name"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "admin?",          default: false
   end
 
   add_foreign_key "applications", "users"
