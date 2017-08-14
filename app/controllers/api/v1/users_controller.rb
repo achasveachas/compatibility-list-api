50182b04a65477bf79f6ca483ff584077b5ed883
Yechiel Kalmenson <contact@yechiel.me>
2017-08-14 16:53:15 -0400

    Added admin to user strong_params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -28,6 +28,6 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :username, :password)
+    params.require(:user).permit(:name, :username, :password, :admin)
   end
 end
