7bed1ca9316ac0614f0541ed2700b3f2b156ea37
Yechiel Kalmenson <contact@yechiel.me>
2017-10-25 15:22:12 -0400

    create script in db/seed to migrate notes into comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-@user = User.create(name: "Test User", username: "testuser", password: "testtest")
+# @user = User.create(name: "Test User", username: "testuser", password: "testtest")
 
-User.all.each do |user|
-  3.times do |i|
-    user.applications.create(software: "Software number #{i}")
-  end
+# User.all.each do |user|
+#   3.times do |i|
+#     user.applications.create(software: "Software number #{i}")
+#   end
+# end
+
+Application.all.each do |app|
+  app.comments.create(body: app.notes, user: User.last)
 end
