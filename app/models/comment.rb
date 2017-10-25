12203033b7b389a813d3a1e3159fdc2d06e4ae2e
Yechiel Kalmenson <contact@yechiel.me>
2017-10-25 14:38:16 -0400

    add comment model and table

---
+++ app/models/comment.rb
@@ -0,0 +1,4 @@
+class Comment < ApplicationRecord
+  belongs_to :application
+  belongs_to :user
+end
---
+++ spec/factories/comments.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :comment do
+    body "MyString"
+    application nil
+    user nil
+  end
+end
