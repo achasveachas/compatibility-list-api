b9278cadaaa74dcce1bb6c96f9354501d68e3c61
Yechiel Kalmenson <contact@yechiel.me>
2017-10-25 14:48:53 -0400

    add relationship test for application has_many relationship

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -2,11 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Application, type: :model do
   describe 'relationships' do
-    it 'belongs to a user' do
-      user = create(:user)
-      app = user.applications.create(software: "CardKnox")
 
-      expect(app.user.id).to eq(user.id)
+    before(:each) do
+      @user = create(:user)
+      @app = @user.applications.create(software: "CardKnox")
+      @comment = @app.comments.create(body: "Comment Body")
     end
+
+    it 'belongs to a user' do
+      expect(@app.user.id).to eq(@user.id)
+    end
+
+    it 'has many comments' do
+      expect(@app.comments.last.id).to eq(@comment.id)
+    end 
   end
 end
