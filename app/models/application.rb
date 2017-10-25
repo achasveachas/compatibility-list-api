28fb2995d341721dec74e972d7cf449386b412c0
Yechiel Kalmenson <contact@yechiel.me>
2017-10-25 15:17:01 -0400

    add comment associations to user and applications

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,3 +1,4 @@
 class Application < ApplicationRecord
   belongs_to :user, optional: true
+  has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :applications
+  has_many :comments
   has_secure_password
 
   validates :username, presence: true
