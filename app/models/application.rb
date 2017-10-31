a594779bc5b5fe2884a34bc1f3f0e7792fe0fb87
Yechiel Kalmenson <contact@yechiel.me>
2017-10-31 12:39:13 -0400

    add validations to prevent applications without comments

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,5 @@
 class Application < ApplicationRecord
   belongs_to :user, optional: true
   has_many :comments, dependent: :destroy
+  validates_presence_of :comments, :software
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :application
   belongs_to :user
+  validates_presence_of :body
 end
