515b11c39c6c18d06cd093219721ac12d938ee70
Yechiel Kalmenson <contact@yechiel.me>
2017-11-09 12:18:20 -0500

    add functionality to automatically mark compatible/incompatible based on the gateway choices

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,4 +2,16 @@ class Application < ApplicationRecord
   belongs_to :user, optional: true
   has_many :comments, dependent: :destroy
   validates_presence_of :comments, :software
+
+  before_save :determine_compatibility
+
+  def is_compatible?
+    self.omaha || self.nashville || self.buypass || self.north || self.elavon || self.tsys
+  end
+
+
+  def determine_compatibility
+    self.compatible = self.is_compatible?
+  end
+
 end
