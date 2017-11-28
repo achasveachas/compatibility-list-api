7af70a3be934afa6238139131fbbae04da088fca
Yechiel Kalmenson <contact@yechiel.me>
2017-11-28 16:52:25 -0500

    add validation specs

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Application, type: :model do
   before(:each) do
     @user = User.create(username: Faker::Internet.user_name, password: Faker::Internet.password)
     @app = @user.applications.new(software: "CardKnox", omaha: true)
+    @invalid_app = @user.applications.new
     @comment = @app.comments.build(body: "Comment Body", user: @user)
     @incompatible_app = @user.applications.new(software: "BMS", omaha: false, nashville: false, north: false, buypass: false, elavon: false, tsys: false)
     @incompatible_app.comments.build(body: "Comment Body", user: @user)
@@ -20,6 +21,20 @@ RSpec.describe Application, type: :model do
     it 'has many comments' do
       expect(@app.comments.last.id).to eq(@comment.id)
     end 
+  end
+
+  describe 'validations' do
+
+    it 'validates presence of software' do
+      expect(@invalid_app).not_to be_valid
+      expect(@invalid_app.errors.full_messages).to include("Software can't be blank")
+    end
+
+    it 'validates presence of comments' do
+      expect(@invalid_app).not_to be_valid
+      expect(@invalid_app.errors.full_messages).to include("Comments can't be blank")
+    end
+
   end
 
   describe 'attributes' do
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,13 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+
+  before(:each) do
+    @user = User.create(username: Faker::Internet.user_name, password: Faker::Internet.password)
+    @app = Application.new(software: "CardKnox")
+    @comment = @app.comments.build(body: "Comment Body", user: @user)
+    @invalid_comment = @app.comments.build(user: @user)
+  end
   describe 'relationships' do
 
-    before(:each) do
-      @user = User.create(username: Faker::Internet.user_name, password: Faker::Internet.password)
-      @app = Application.new(software: "CardKnox")
-      @comment = @app.comments.build(body: "Comment Body", user: @user)
-    end
+
 
     it 'belongs to a user' do
       expect(@comment.user).to be_a(User)
@@ -16,5 +19,14 @@ RSpec.describe Comment, type: :model do
     it 'belongs to an application' do
       expect(@comment.application).to be_a(Application)
     end 
+  end
+
+  describe 'validations' do
+
+    it 'validates presence of body' do
+      expect(@invalid_comment).not_to be_valid
+      expect(@invalid_comment.errors.full_messages).to include("Body can't be blank")
+    end
+
   end
 end
